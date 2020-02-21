3b986cf0c3ea6f69355b6c5e91c6e46bda62acc2
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-21 11:07:44 -0700

    Add review text

--- spec/features/user_can_search_for_a_book_spec.rb
+++ spec/features/user_can_search_for_a_book_spec.rb
@@ -10,8 +10,15 @@ RSpec.describe 'User can search for a book' do
     expect(page).to have_css('.title')
     expect(page).to have_content('Title: The Man Who Saw Everything')
     expect(page).to have_css('.author')
-    expect(page).to have_content('.genres')
 
-    expect(page).to have_css('.reviews', count: 2)
+    #if I had time I would want to go back and handle books with multiple authors
+    expect(page).to have_content('Author: Deborah Levy')
+
+    review_1 = "“The Man Who Saw Everything,” which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at “authoritarian old men.”"
+    review_2 = "Deborah Levy’s latest novel, “The Man Who Saw Everything,” experiments with time travel, history and the endless complications of love."
+
+    expect(page).to have_css('.review', count: 2)
+    expect(page).to have_content(review_1)
+    expect(page).to have_content(review_2)
   end
 end
