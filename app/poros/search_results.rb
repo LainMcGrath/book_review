31c9799e417cede002b4e4f224a691a3c6af99e8
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-21 11:10:13 -0700

    add ny times review methods

--- app/poros/search_results.rb
+++ app/poros/search_results.rb
@@ -5,18 +5,33 @@ class SearchResults
     @book_title = book_title
   end
 
-  def get_data
+  def get_book_data
     BookFetcher.new.book_response(book_title)
   end
 
+  def get_review_data
+    BookFetcher.new.book_review_response(book_title)
+  end
+
   def book_results
-    data = get_data
+    data = get_book_data
     create_book(data)
   end
 
   def create_book(data)
     data[:docs].map do |book_info|
       Book.new(book_info)
+    end
+  end
+
+  def book_review
+    data = get_review_data
+    create_review(data)
+  end
+
+  def create_review(data)
+    data[:results].map do |book_info|
+      Review.new(book_info)
     end
   end
 end
