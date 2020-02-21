56043fb38a490192687e43ab18944fa009c0753a
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-21 11:28:57 -0700

    Edit to pass in ENV variable

--- app/poros/book_fetcher.rb
+++ app/poros/book_fetcher.rb
@@ -19,10 +19,7 @@ class BookFetcher
  def book_review_response(book_title)
    conn = ny_times_connection
    response = conn.get("svc/books/v3/reviews.json") do |req|
-     # everytime I pass the API key in with an ENV variable, I receive a failed response???
-     # hard keyed for the sake of time
-     # req.params['api-key'] = ENV['ny_times_api_key']
-     req.params['api-key'] = 'RNd9svtLddzX5F8rreYcdHYBE3CbfWLA'
+     req.params['api-key'] = ENV['ny_times_api_key']
      req.params['title'] = book_title
    end
    review_parse(response)
